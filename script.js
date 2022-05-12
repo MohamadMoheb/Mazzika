e375cb4fb05d7a309432f1326d035515275f17f0
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-05-13 00:26:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-document.getElementById('Previous-Song').addEventListener('click', PreviousSong);
-document.getElementById('Play-Pause').addEventListener('click', PlayPause);
-document.getElementById('Next-Song').addEventListener('click', NextSong);
+document.getElementById('PlayPrevious').addEventListener('click', PlayPrevious);
+document.getElementById('PlayPause').addEventListener('click', PlayPause);
+document.getElementById('PlayNext').addEventListener('click', PlayNext);
 
-document.getElementById('tester').addEventListener('click', tester);
-
-function importtester() {
-    document.getElementById('result').innerHTML = 'Hello World';
+function PlayPrevious() {
+    document.getElementById('result').innerHTML = 'Previous';
 }
 
-function removetester() {
-    document.getElementById('result').innerHTML = '';
+function PlayPause() {
+    document.getElementById('result').innerHTML = 'Play Pause';
+}
+
+function PlayNext() {
+    document.getElementById('result').innerHTML = 'Next';
 }
