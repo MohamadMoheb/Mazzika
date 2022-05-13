243695358973c62fcc4cc77fe929f6b4ee9b910f
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-05-14 00:59:02 +0300

    Create controls.js

---
+++ controls.js
@@ -0,0 +1,37 @@
+document.getElementById('PlayPrevious').addEventListener('click', PlayPrevious);
+document.getElementById('PlayNext').addEventListener('click', PlayNext);
+document.getElementById('PlayPause').addEventListener('click', PlayPause);
+
+var anchorPoint = 0;
+var play = newBoolean(false);
+
+
+function PlayPrevious() {
+    --anchorPoint;
+    console.log("Playing Previous Song");
+    document.getElementById('result').innerHTML = anchorPoint;
+}
+
+function PlayNext() {
+    ++anchorPoint;
+    console.log("Playing Next Song");
+    document.getElementById('result').innerHTML = anchorPoint;
+}
+
+function PlayPause() {
+    if (play == false)
+    {
+        document.getElementById('playpausestate').innerHTML = 'Play';
+        play = true
+
+        anchorPoint = -0
+    }
+    
+    else
+    {
+        document.getElementById('playpausestate').innerHTML = 'Pause';
+        play =  false
+
+        anchorPoint = 0
+    }
+}
