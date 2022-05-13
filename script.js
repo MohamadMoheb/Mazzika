0a4938486696da5bcc66131cff41e39efa41fa67
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-05-13 14:32:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,32 @@ document.getElementById('PlayPrevious').addEventListener('click', PlayPrevious);
 document.getElementById('PlayPause').addEventListener('click', PlayPause);
 document.getElementById('PlayNext').addEventListener('click', PlayNext);
 
+var P = newBoolean(true);
+
 function PlayPrevious() {
     document.getElementById('result').innerHTML = 'Previous';
 }
 
 function PlayPause() {
-    document.getElementById('result').innerHTML = 'Play Pause';
+    if (P = false)
+    {
+        document.getElementById('result').innerHTML = 'Play';
+        P = true
+    }
+    
+    else if(P = true)
+    {
+        document.getElementById('result').innerHTML = 'Pause';
+    }
 }
 
 function PlayNext() {
     document.getElementById('result').innerHTML = 'Next';
 }
+
+
+/*
+function PlayPause() {
+    document.getElementById('result').innerHTML = 'Play Pause';
+}
+*/
