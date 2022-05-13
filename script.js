626d3b9c68a42f4b970a7200181cfafde3487a1e
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-05-13 14:40:29 +0300

    Update script.js
    Implemented toggle play/pause button

--- script.js
+++ script.js
@@ -2,32 +2,26 @@ document.getElementById('PlayPrevious').addEventListener('click', PlayPrevious);
 document.getElementById('PlayPause').addEventListener('click', PlayPause);
 document.getElementById('PlayNext').addEventListener('click', PlayNext);
 
-var P = newBoolean(true);
+var P = newBoolean(false);
+
+function PlayPause() {
+    if (P == false)
+    {
+        document.getElementById('result').innerHTML = 'Play';
+        P = false
+    }
+    
+    else
+    {
+        document.getElementById('result').innerHTML = 'Pause';
+        P =  true
+    }
+}
 
 function PlayPrevious() {
     document.getElementById('result').innerHTML = 'Previous';
 }
 
-function PlayPause() {
-    if (P = false)
-    {
-        document.getElementById('result').innerHTML = 'Play';
-        P = true
-    }
-    
-    else if(P = true)
-    {
-        document.getElementById('result').innerHTML = 'Pause';
-    }
-}
-
 function PlayNext() {
     document.getElementById('result').innerHTML = 'Next';
 }
-
-
-/*
-function PlayPause() {
-    document.getElementById('result').innerHTML = 'Play Pause';
-}
-*/
