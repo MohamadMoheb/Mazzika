fc66be36aceb53ee81e67c2efda115838a242438
Mohamad Moheb <mohamadmohebx@gmail.com>
2022-05-14 03:51:50 +0300

    Update controls.js

--- controls.js
+++ controls.js
@@ -5,31 +5,37 @@ document.getElementById('PlayPause').addEventListener('click', PlayPause);
 var trackIndex = 0;
 var play;
 
+var audio = new Audio("folder_name/audio_file.mp3");
 
 function PlayPrevious() {
     trackIndex--;
-    console.log("Playing Previous Song");
+    console.log("Playing Previous Track");
     document.getElementById('result').innerHTML = trackIndex;
 }
 
 function PlayNext() {
     trackIndex++;
-    console.log("Playing Next Song");
+    console.log("Playing Next Track");
     document.getElementById('result').innerHTML = trackIndex;
 }
 
 function PlayPause() {
+
+    trackIndex = 0
+
+    console.log("Play/Pause Pressed")
+
     if (play == false)
     {
         document.getElementById('playpausestate').innerHTML = 'Play';
         play = true
-        trackIndex = 0
     }
     
     else
     {
         document.getElementById('playpausestate').innerHTML = 'Pause';
         play =  false
-        trackIndex = 0
+        
+        audio.play();
     }
 }
